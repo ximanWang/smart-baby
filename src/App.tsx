3374923bbe5a06d2d4c35449c8f9069a5ae31d3e
= <=>
2023-03-17 16:28:42 +1100

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Baby from "./img/baby.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="max-w-md mx-auto  rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+          <div className="md:flex">
+            <div className="md:shrink-0">
+              <img
+                className="h-48 w-full object-cover md:h-full md:w-48"
+                src={Baby}
+                alt="Baby"
+              />
+            </div>
+            <div className="p-8">
+              <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                Smart Baby
+              </div>
+              <a
+                href="#"
+                className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+              >
+                Incredible Baby
+              </a>
+              <p className="mt-2 text-slate-500">
+                This is a smart baby. This is a smart baby. This is a smart
+                baby. This is a smart baby. This is a smart baby.
+              </p>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
